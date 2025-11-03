ba807fe7cfee2471b98044317bbaf524d13fcab2
Eranga <erangakavishanka@icloud.com>
2025-11-03 16:25:05 +0530

    developed welcome scrren

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -6,9 +6,32 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(image: NetworkImage('https://cdni.iconscout.com/illustration/premium/thumb/online-education-2890098-2403435.png')),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Online Learning Platform', style: TextStyle(color: Colors.purple, fontSize: 30, fontWeight: FontWeight.bold),),
+              ),
+              Text('Your learning adventure starts now! Explore lessons, gain skills, and take one step closer to success today', style: TextStyle(color:Colors.grey.shade700, fontSize: 20, fontWeight: FontWeight.w400, ), textAlign: TextAlign.center,)
+          
+          
+          
+            ],
+            
+          
+          
+          ),
+        ),
+      )
 
 
-      
+
     );
   }
 }
