a0140ef6f55d20c907d2dfd30212638c9639c7e1
Eranga <erangakavishanka@icloud.com>
2025-11-03 16:07:28 +0530

    finished making splash scrren transions

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,11 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
-class  SplashScreen extends StatelessWidget {
+import 'welcome_screen.dart';
+
+class  SplashScreen extends StatefulWidget {
   const  SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Logger().e("message");
+    Timer(Duration(seconds: 3), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => WelcomeScreen()));
+    });
+
+  }
+  @override
   Widget build(BuildContext context) {
+
+
+
+
+
     return Scaffold(
 
       backgroundColor: Colors.purple,
